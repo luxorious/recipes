e0319a3036f7b29120ef4cfcd1b0537e7182efc8
luxorious <vovan_194@mail.ru>
2023-08-06 13:17:29 +0200

    create entities

---
+++ src/main/java/com/recipes/entity/Receipt.java
@@ -0,0 +1,76 @@
+package com.recipes.entity;
+
+import com.recipes.entity.enumerations.Categories;
+import com.recipes.entity.enumerations.Countries;
+import com.recipes.entity.enumerations.DishType;
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.sql.Timestamp;
+import java.util.List;
+
+@Entity
+@Table(name = "receipt")
+@NoArgsConstructor
+@Data
+public class Receipt {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "user_id")
+    private Long userId;
+
+    @Column(name = "category_id")
+    private Long categoryId;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "instruction")
+    private String instruction;
+
+    @Column(name = "ingredients")
+    private String ingredients;
+
+    @Column(name = "cooking_time")
+    private Long cookingTime;
+
+    @Column(name = "rating")
+    private Double rating;
+
+    @Column(name = "country")
+    @Enumerated(EnumType.STRING)
+    private Countries country;
+
+    @Column(name = "dish_type")
+    @Enumerated(EnumType.STRING)
+    private DishType dishType;
+
+    @Column(name = "image")
+    private String imageLink;
+
+    @Column(name = "categories")
+    @Enumerated(EnumType.STRING)
+    private List<Categories> categories;
+
+    @Column(name = "created_at")
+    @Temporal(TemporalType.TIMESTAMP)
+    @CreationTimestamp
+    private Timestamp createdAt;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", referencedColumnName = "id", insertable = false, updatable = false)
+    private User user;
+
+//    @OneToMany(mappedBy = "category", cascade = CascadeType.ALL, orphanRemoval = true)
+//    private List<Category> categories;
+
+
+}
---
+++ src/main/java/com/recipes/entity/User.java
@@ -0,0 +1,28 @@
+package com.recipes.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+@NoArgsConstructor
+@Data
+public class User {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    private List<Receipt> receipts;
+}
