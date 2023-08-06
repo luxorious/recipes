776f6c71fe4d954b5285ec1a53d6a3adad6c86ca
luxorious <vovan_194@mail.ru>
2023-08-06 13:19:39 +0200

    create repository

---
+++ src/main/java/com/recipes/repository/ReceiptRepository.java
@@ -0,0 +1,9 @@
+package com.recipes.repository;
+
+import com.recipes.entity.Receipt;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReceiptRepository extends JpaRepository<Receipt, Long> {
+}
