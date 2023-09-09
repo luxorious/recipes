081afbf9f6249881e4458258162df61b4b8d0b4d
luxorious <vovan_194@mail.ru>
2023-09-10 00:40:41 +0200

    create entities

---
+++ src/main/java/com/recipes/entity/MeasureUnit.java
@@ -0,0 +1,19 @@
+package com.recipes.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "measure_units")
+@Data
+@NoArgsConstructor
+public class MeasureUnit {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "unit", length = 32)
+    private String unit;
+
+}
