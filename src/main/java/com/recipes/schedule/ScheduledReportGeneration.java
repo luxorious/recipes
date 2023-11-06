9bfab80fb2e862498f37fa2831dbf35aefebfa46
luxorious <vovan_194@mail.ru>
2023-11-06 21:53:30 +0100

    Add scheduled multi-thread report generation and sending

--- src/main/java/com/recipes/schedule/ScheduledReportGeneration.java
+++ src/main/java/com/recipes/schedule/ScheduledReportGeneration.java
@@ -2,7 +2,17 @@ package com.recipes.schedule;
 
 public interface ScheduledReportGeneration {
 
-    void generation();
+    /**
+     * Scheduled method for generating reports
+     * Method use another Thread and save report in ConcurrentHashMap
+     * Scheduled is midnight every month
+     */
+    void generateReports();
 
-    void send();
+    /**
+     * Scheduled method for sending reports (using another thread)
+     * Send report retrieves data (mail and message text) from Map and
+     * sends them at midday on the 1st day of each month.
+     */
+    void sendReports();
 }
